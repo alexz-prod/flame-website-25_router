cf8c124126a8ea2e688a79a3138bbbe0ec07f594
derzeiss <derzeiss@users.noreply.github.com>
2026-01-02 10:23:18 +0100

    Remove YearOverview pre-titles

--- app/sections/YearOverview.tsx
+++ app/sections/YearOverview.tsx
@@ -12,9 +12,6 @@ export default function () {
 
       <ul className="gap-content with-border-top relative mt-6 grid pt-4 sm:gap-12 lg:mt-12 lg:ml-auto lg:w-[80%] lg:grid-cols-3">
         <li>
-          <div className="text-pray font-semibold tracking-wide uppercase">
-            Wer ist Gott?
-          </div>
           <div className="h3 mb-4">Gottes&shy;bild</div>
           <p>
             Nur wenn du weißt, wer Gott ist, kommt dein Leben in eine gesunde
@@ -26,10 +23,6 @@ export default function () {
           </p>
         </li>
         <li>
-          <div className="text-pray font-semibold tracking-wide uppercase">
-            Identität &amp; Beziehungen
-          </div>
-
           <div className="h3 mb-4">Selbst&shy;bild</div>
           <p>
             Wer bist du eigentlich? Warum hat Gott dich erschaffen und will
@@ -40,10 +33,6 @@ export default function () {
           </p>
         </li>
         <li>
-          <div className="text-pray font-semibold tracking-wide uppercase">
-            Was ist meine Berufung?
-          </div>
-
           <div className="h3 mb-4">Welt&shy;bild</div>
           <p>
             Du bist für mehr gemacht, als einen Wohlfühl-Glauben, der sich v.a.
